60b0bf9084b431ccb15d929a5ffe302d0c120a1d
britishpop <drmakc@gmail.com>
2019-05-30 09:54:42 +0300

    phone catalogue done

--- databases/work_with_database/phones/management/commands/import_phones.py
+++ databases/work_with_database/phones/management/commands/import_phones.py
@@ -2,6 +2,7 @@ import csv
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
+from django.utils.text import slugify
 
 
 class Command(BaseCommand):
@@ -12,9 +13,15 @@ class Command(BaseCommand):
         with open('phones.csv', 'r') as csvfile:
 
             phone_reader = csv.reader(csvfile, delimiter=';')
-            # пропускаем заголовок
             next(phone_reader)
 
             for line in phone_reader:
-                # TODO: Добавьте сохранение модели
-                pass
+                Phone.objects.create(
+                        id = line[0],
+                        name = line[1],
+                        image = line[2],
+                        price = line[3],
+                        release_date = line[4],
+                        lte_exists = line[5],
+                        slug = slugify(line[1])
+                )
