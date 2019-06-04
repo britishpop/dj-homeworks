30092cbba30628129f4a9ea28ee78853c4c914e0
britishpop <drmakc@gmail.com>
2019-06-04 08:36:42 +0300

    add localhost to ALLOWED_HOSTS

--- request-handling/pagination/app/settings.py
+++ request-handling/pagination/app/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
