d735079c3060f12ae202478f83430b359e808639
britishpop <drmakc@gmail.com>
2019-06-07 18:15:27 +0300

    students finished

--- databases_2/orm_migrations/school/views.py
+++ databases_2/orm_migrations/school/views.py
@@ -6,3 +6,7 @@ from .models import Student
 class StudentListView(ListView):
     model = Student
     ordering = 'group'
+
+    def get_queryset(self):
+        queryset = Student.objects.prefetch_related('teacher')
+        return queryset
