c7c18b4ce26fdfa030e631b7057ad40dc77d5dc6
britishpop <drmakc@gmail.com>
2019-06-27 15:22:23 +0300

    create application done

--- creating-project/application/table/views.py
+++ creating-project/application/table/views.py
@@ -1,21 +1,24 @@
-import csv
-
 from django.shortcuts import render
+from .models import TableField, TableFile
+import csv, os
 
 
-CSV_FILENAME = 'phones.csv'
-COLUMNS = [
-    {'name': 'id', 'width': 1},
-    {'name': 'name', 'width': 3},
-    {'name': 'price', 'width': 2},
-    {'name': 'release_date', 'width': 2},
-    {'name': 'lte_exists', 'width': 1},
-]
+# CSV_FILENAME = 'phones.csv'
+# COLUMNS = [
+#     {'name': 'id', 'width': 1},
+#     {'name': 'name', 'width': 3},
+#     {'name': 'price', 'width': 2},
+#     {'name': 'release_date', 'width': 2},
+#     {'name': 'lte_exists', 'width': 1},
+# ]
 
 
 def table_view(request):
     template = 'table.html'
-    with open(CSV_FILENAME, 'rt') as csv_file:
+    table_file = TableFile.objects.get(pk=1).get_path()
+    fields = TableField.objects.all().order_by('position')
+
+    with open(table_file, 'rt') as csv_file:
         header = []
         table = []
         table_reader = csv.reader(csv_file, delimiter=';')
@@ -28,9 +31,9 @@ def table_view(request):
                 table.append(row)
 
         context = {
-            'columns': COLUMNS, 
+            'columns': fields, 
             'table': table, 
-            'csv_file': CSV_FILENAME
+            'csv_file': os.path.basename(table_file)
         }
         result = render(request, template, context)
     return result
